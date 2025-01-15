1f6aa3566655b20a133cb90c49729ddd99ebd00d
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-15 08:02:00 +0700

    fix: prisma adapter

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,3 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
 
-export const prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaNeon(neon);
+  prisma = new PrismaClient({ adapter });
+} else {
+  prisma = new PrismaClient();
+}
+
+export { prisma };
