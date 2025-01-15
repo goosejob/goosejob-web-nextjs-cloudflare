d62cd3dca62c0ffa1537d35237758c7b28d8a04d
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-15 07:48:17 +0700

    fix: build script

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,16 +1,3 @@
-import { Pool } from "@prisma/pg-worker";
-import { PrismaPg } from "@prisma/adapter-pg-worker";
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  const connectionString = `${process.env.DATABASE_URL}`;
-  const pool = new Pool({ connectionString });
-  const adapter = new PrismaPg(pool);
-  prisma = new PrismaClient({ adapter });
-} else {
-  prisma = new PrismaClient();
-}
-
-export { prisma };
+export const prisma = new PrismaClient();
