68b7b6b4af64a8978201d3085dad4cdf6f711c1f
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-15 08:29:28 +0700

    fix: prisma adapter

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,3 +1,30 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
 
-export const prisma = new PrismaClient();
+neonConfig.webSocketConstructor = ws;
+
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+neonConfig.poolQueryViaFetch = true;
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+let prisma: PrismaClient;
+
+if (
+  process.env.NODE_ENV === "development" ||
+  process.env.NEXTJS_ENV === "development"
+) {
+  console.log("Development");
+
+  prisma = new PrismaClient();
+} else {
+  console.log("Production");
+
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaNeon(pool);
+  prisma = new PrismaClient({ adapter });
+}
+
+export { prisma };
